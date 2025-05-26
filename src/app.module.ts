1d65fb4ce4054a2b322faea3be4b1f7f5b37ce7d
AZIZ <bozorboyevazizjon56@gmail.com>
2025-05-26 17:10:29 +0500

    create frist commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+
+
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
+  providers: [ ],
+})
+export class AppModule {}
